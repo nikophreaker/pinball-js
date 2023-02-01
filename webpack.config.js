a84ba406244135a379c7c88ec01332f0bb4f21c2
nikophreaker <nikx449@gmail.com>
2023-02-01 15:01:03 +0700

    update dist webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 import WebpackObfuscator from "webpack-obfuscator"
+import path from 'path';
+import {
+    fileURLToPath
+} from 'url';
+const __filename = fileURLToPath(
+    import.meta.url);
+const __dirname = path.dirname(__filename);
+const DIST_DIR = path.join(__dirname, '/src/js/');
 
 export default {
 
@@ -16,11 +24,11 @@ export default {
 
     // output file
     output: {
-
         // file name
         filename: "bundle.js",
 
         // complete path
-        // path: __dirname
+        path: DIST_DIR
+
     }
 }
